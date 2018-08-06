d62afc5815cd717fa994213d412eed9f8f9d602c
Joanna <jhazimih@gmail.com>
2018-08-06 11:16:33 -0400

    setting up templating

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var nunjucksRender = require('gulp-nunjucks-render');
+var data = require('gulp-data');
+
 
 gulp.task('browserSync', function() {
   browserSync.init({
@@ -18,6 +21,18 @@ gulp.task('sass', function() {
     .pipe(browserSync.reload({
       stream: true
     }))
+});
+
+gulp.task('templates', function() {
+  return gulp.src('app/pages/**/*.+(html|nunjucks)')
+    // Adding data to Nunjucks
+    .pipe(data(function() {
+      return require('./app/data.json')
+    }))
+    .pipe(nunjucksRender({
+      path: ['app/templates']
+    }))
+    .pipe(gulp.dest('app'))
 });
 
 gulp.task('watch', ['browserSync', 'sass'], function (){
